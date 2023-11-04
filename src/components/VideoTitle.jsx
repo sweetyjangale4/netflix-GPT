949075fee67751de865ab6315baa2c44b5451c41
sweetyjangale4 <sweetyvjangale.com>
2023-11-04 15:17:10 +0530

    MovieDetailsCard added && mui-icons added

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import InfoIcon from '@mui/icons-material/Info';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const VideoTitle = ({title,overview}) => {
   return (
@@ -6,8 +8,8 @@ const VideoTitle = ({title,overview}) => {
       <h1 className='text-6xl font-bold'>{title}</h1>
       <p className='py-6 text-lg w-1/4'>{overview}</p>
       <div>
-        <button className='bg-white text-black p-4 px-12 text-xl rounded-lg hover:bg-opacity-80'> ▶ Play</button>
-        <button className='mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg'>More Info</button>
+        <button className='bg-white text-black p-4 px-12 text-xl rounded-lg hover:bg-opacity-80'><PlayArrowIcon className='text-black'/> Play</button>
+        <button className='mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg'><InfoIcon className='text-white'/> More Info</button>
       </div>
     </div>
   );
